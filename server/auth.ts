587b794ae752eae2130c568c2dee126b88511ca2
Eclassic32 <eclassic32004@gmail.com>
2026-07-26 17:08:08 +0500

    [WIP] Auth with discord

--- server/auth.ts
+++ server/auth.ts
@@ -1,3 +1,6 @@
+import { type Socket, type Server } from 'socket.io';
+import express from 'express';
+
 export type User = {
     id: string;
     name: string;
@@ -10,4 +13,27 @@ export const placeholderUser = (id: number): User => ({
     image: `https://example.com/user${id}.png`
 });
 
-export const currentUser = placeholderUser(20); // Placeholder for the current user
+const SERVER_BASE_URL = process.env.SERVER_BASE_URL || 'http://localhost:3000';
+const DISCORD_API_URL = 'https://discord.com/api/v10';
+const DISCORD_APP_ID = process.env.DISCORD_APP_ID;
+const DISCORD_APP_SECRET = process.env.DISCORD_APP_SECRET;
+
+export function setupAuthExpress(app: express.Application) {
+    app.get('/api/auth/discord', (req, res) => {
+        const redirectUri = `${SERVER_BASE_URL}/api/auth/discord/callback`;
+        const discordAuthUrl = `${DISCORD_API_URL}/oauth2/authorize?client_id=${DISCORD_APP_ID}&redirect_uri=${encodeURIComponent(redirectUri)}&response_type=code&scope=identify`;
+        res.redirect(discordAuthUrl);
+    });
+
+    app.get('/api/auth/discord/callback', async (req, res) => {
+        const code = req.query.code as string;
+        const redirectUri = `${SERVER_BASE_URL}/api/auth/discord/callback`;
+
+
+    });
+
+}
+
+export function setupAuthSocket(socket: Socket, _io: Server) {
+    
+}
--- src/components/TopNavBar.vue
+++ src/components/TopNavBar.vue
@@ -1,7 +1,17 @@
 <script setup lang="ts">
+import { RouterLink } from 'vue-router';
+import { ref } from 'vue';
+
+const loginBtn = ref<HTMLButtonElement | null>(null);
 
 </script>
 <template>
   <nav>
+    <RouterLink to="/">Home</RouterLink>
+    <RouterLink to="/rooms">Rooms</RouterLink>
+    <button id="login-btn" ref="loginBtn">Login</button>
   </nav>
 </template>
+<style scoped>
+
+</style>
